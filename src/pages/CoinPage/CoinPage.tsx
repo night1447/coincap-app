671f4b98436981392785475a26850ab4bc07f98a
Дмитрий Солодов <dmitrijsolodov@MacBook-Pro-Dmitrij.local>
2023-08-07 09:41:32 +0300

    create routing

---
+++ src/pages/CoinPage/CoinPage.tsx
@@ -0,0 +1,9 @@
+const CoinPage = () => {
+    console.log(123);
+    return (
+        <div>
+
+        </div>
+    );
+};
+export default CoinPage;
---
+++ src/routing/index.ts
@@ -0,0 +1,7 @@
+import {createBrowserRouter} from "react-router-dom";
+import routes from "./routes.tsx";
+
+
+const index = createBrowserRouter(routes);
+
+export default index;
