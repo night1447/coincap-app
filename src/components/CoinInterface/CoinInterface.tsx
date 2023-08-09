65ccbf99ea7b51f16d7134904fd9451b6e5feff5
Дмитрий Солодов <dmitrijsolodov@MacBook-Pro-Dmitrij.local>
2023-08-09 14:21:18 +0300

    adaptation for mobile

--- src/components/CoinInterface/CoinInterface.tsx
+++ src/components/CoinInterface/CoinInterface.tsx
@@ -1,15 +1,13 @@
 import { FC, useEffect, useState } from 'react';
 import Section from '../../UI/Section/Section.tsx';
-import Typography from '../../UI/Typography/Typography.tsx';
 import styles from './coin.module.scss';
 import { ICurrency } from '../../models';
-import getCurrency from '../../utils/getCurrency.ts';
 import Graphic from '../Graphic/Graphic.tsx';
 import $api from '../../api';
 import Button from '../../UI/Button/Button.tsx';
 import { useNavigate } from 'react-router';
-import getStylePriceDifference from '../../utils/getStylePriceDifference.ts';
 import BuyingInterface from '../BuyingInterface/BuyingInterface.tsx';
+import Head from './Head/Head.tsx';
 
 interface CoinProps {
   id: string;
@@ -40,63 +38,17 @@ const CoinInterface: FC<CoinProps> = ({ id }) => {
 
   return (
       <Section>
-        <Button
-            type={'button'}
-            variant={'error'}
-            onClick={goBackHandler}
-            className={styles.back}
-        >
-          Назад
-        </Button>
-        <div className={styles.head}>
-          <div className={styles.inner}>
-            <div className={styles.rank}>
-              <span className={styles.rank_bold}>{coin?.rank}</span>
-              <span>Rank</span>
-            </div>
-            <div>
-              <Typography type={'h1'} className={styles.name}>
-                {coin.name} ({coin.symbol})
-              </Typography>
-              <Typography type={'h2'} className={styles.price}>
-                {coin.priceUsd}
-                {getCurrency()}
-                <span
-                    className={`${styles.price_value} ${
-                        getStylePriceDifference(coin.changePercent24Hr.toString())
-                    }`}
-                >
-                {coin.changePercent24Hr}%
-              </span>
-              </Typography>
-            </div>
-          </div>
-          <ul className={styles.info}>
-            <li className={styles.item}>
-              <Typography type={'p'}>Market cap</Typography>
-              <Typography type={'h3'} className={styles.value}>
-                {coin.marketCapUsd}
-                {getCurrency()}
-              </Typography>
-            </li>
-            <li className={styles.item}>
-              <Typography type={'p'}>Volume (24Hr)</Typography>
-              <Typography type={'h3'} className={styles.value}>
-                {coin.volumeUsd24Hr}
-                {getCurrency()}
-              </Typography>
-            </li>
-            <li className={styles.item}>
-              <Typography type={'p'}>Supply</Typography>
-              <Typography type={'h3'} className={styles.value}>
-                {coin.supply}
-                {getCurrency()}
-              </Typography>
-            </li>
-          </ul>
-        </div>
-        <Graphic id={id} className={styles.graphic}/>
-        <BuyingInterface coin={coin}/>
+          <Button
+              type={'button'}
+              variant={'error'}
+              onClick={goBackHandler}
+              className={styles.back}
+          >
+              Назад
+          </Button>
+          <Head coin={coin} />
+          <Graphic id={id} className={styles.graphic} />
+          <BuyingInterface coin={coin} />
       </Section>
   );
 };
