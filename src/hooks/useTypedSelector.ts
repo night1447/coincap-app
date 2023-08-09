58ad1f849a67a3888705163e378340717a1b8d53
Дмитрий Солодов <dmitrijsolodov@MacBook-Pro-Dmitrij.local>
2023-08-09 16:46:09 +0300

    delete hooks

--- src/hooks/useTypedSelector.ts
+++
@@ -1,7 +0,0 @@
-import { rootReducerType } from '../store/reducers';
-import { TypedUseSelectorHook, useSelector } from 'react-redux';
-
-
-const useTypedSelector: TypedUseSelectorHook<rootReducerType> = useSelector;
-export default useTypedSelector;
-
